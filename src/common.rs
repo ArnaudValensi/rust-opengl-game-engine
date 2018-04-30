52ef7ac1ade46c1a707d4c4cf3fd78b0f487799d
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-04-30 21:16:27 +0200

    Improve window abstraction

--- src/common.rs
+++ src/common.rs
@@ -1,15 +1,56 @@
 extern crate glutin;
+extern crate gl;
 
-use self::glutin::{Event, WindowEvent, EventsLoop, GlWindow, GlContext};
+use self::glutin::{
+    WindowBuilder,
+    ContextBuilder,
+    Event,
+    WindowEvent,
+    EventsLoop,
+    GlWindow,
+    GlContext
+};
 
-pub fn process_events(events_loop: &mut EventsLoop, running: &mut bool, gl_window: &GlWindow) {
-    events_loop.poll_events(|event| {
+pub struct Window {
+    pub gl_window: glutin::GlWindow,
+    pub running: bool,
+    events_loop: glutin::EventsLoop,
+}
+
+impl Window {
+    pub fn new(width: u32, height: u32) -> Window {
+        let events_loop = EventsLoop::new();
+         let window = WindowBuilder::new()
+             .with_title("Hello, world!")
+             .with_dimensions(width, height);
+         let context = ContextBuilder::new()
+             .with_vsync(true);
+         let gl_window = GlWindow::new(window, context, &events_loop).unwrap();
+
+         unsafe {
+             gl_window.make_current().unwrap();
+         }
+
+         gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
+
+         Window {
+             events_loop,
+             gl_window,
+             running: true,
+         }
+    }
+}
+
+pub fn process_events(window: &mut Window) {
+    let running = &mut window.running;
+    let gl_window = &window.gl_window;
+
+    window.events_loop.poll_events(|event| {
         match event {
             Event::WindowEvent{ event, .. } => match event {
                 WindowEvent::Closed => *running = false,
                 WindowEvent::Resized(width, height) => {
                     gl_window.resize(width, height);
-                    // unsafe { gl::Viewport(0, 0, width, height) }
                 },
                 _ => ()
             },
--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,24 @@ extern crate glbs;
 extern crate gl;
 extern crate glutin;
 
-use glbs::common;
+use glbs::common::{Window, process_events};
 use glutin::GlContext;
 
 // settings
 const SCR_WIDTH: u32 = 800;
 const SCR_HEIGHT: u32 = 600;
 
-struct Window {
-    events_loop: glutin::EventsLoop,
-    gl_window: glutin::GlWindow,
-}
-
-impl Window {
-    pub fn new() -> Window {
-        let events_loop = glutin::EventsLoop::new();
-         let window = glutin::WindowBuilder::new()
-             .with_title("Hello, world!")
-             .with_dimensions(SCR_WIDTH, SCR_HEIGHT);
-         let context = glutin::ContextBuilder::new()
-             .with_vsync(true);
-         let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
-
-         Window {
-             events_loop,
-             gl_window,
-         }
-    }
-}
-
 pub fn main() {
     // glfw: initialize and configure
     // ------------------------------
-    let mut window = Window::new();
-
-     unsafe {
-         window.gl_window.make_current().unwrap();
-     }
-
-    gl::load_with(|symbol| window.gl_window.get_proc_address(symbol) as *const _);
+    let mut window = Window::new(SCR_WIDTH, SCR_HEIGHT);
 
     // render loop
     // -----------
-    let mut running = true;
-    while running {
+    while window.running {
         // events
         // -----
-        common::process_events(&mut window.events_loop, &mut running, &window.gl_window);
+        process_events(&mut window);
 
         // render
         // ------
