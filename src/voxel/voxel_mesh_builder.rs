309fad544a58b397a3cbb2c2bfe87f5e0549c295
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-05-30 22:34:58 +0200

    Finish to implement build_mesh

--- src/voxel/voxel_mesh_builder.rs
+++ src/voxel/voxel_mesh_builder.rs
@@ -3,16 +3,15 @@ use super::chunk::Chunk;
 use super::direction::Direction;
 use super::position::Position;
 use super::voxel_geometry::{add_quad_triangles, create_vertex_face};
-// use mesh::Mesh;
 
 pub fn build_mesh(chunk: &Chunk) -> MeshData {
-    let mesh_data = MeshData::new();
+    let mut mesh_data = MeshData::new();
 
     for x in 0..chunk.size_x {
         for y in 0..chunk.size_y {
             for z in 0..chunk.size_z {
                 if chunk.is_solid(x as i64, y as i64, z as i64) {
-                    // meshData = voxel_data(chunk, x, y, z, meshData);
+                    voxel_data(chunk, x as i64, y as i64, z as i64, &mut mesh_data);
                 }
             }
         }
@@ -21,7 +20,7 @@ pub fn build_mesh(chunk: &Chunk) -> MeshData {
     mesh_data
 }
 
-fn voxel_data<'a>(chunk: &Chunk, x: i64, y: i64, z: i64, mesh_data: &'a mut MeshData) -> &'a MeshData {
+fn voxel_data<'a>(chunk: &Chunk, x: i64, y: i64, z: i64, mesh_data: &'a mut MeshData) {
     let position: Position = Position::new(x, y, z);
 
     for direction in Direction::iterator() {
@@ -42,47 +41,4 @@ fn voxel_data<'a>(chunk: &Chunk, x: i64, y: i64, z: i64, mesh_data: &'a mut Mesh
             // mesh_data.uv.Add(colorUv);
         }
     }
-
-    return mesh_data;
 }
-
-// public MeshData BuildMesh(Chunk chunk) {
-//     MeshData meshData = new MeshData();
-//
-//     for (int x = 0; x < chunk.SizeX; x++) {
-//         for (int y = 0; y < chunk.SizeY; y++) {
-//             for (int z = 0; z < chunk.SizeZ; z++) {
-//                 if (chunk.voxels[x, y, z].IsSolid) { // If it is solid
-//                     meshData = Voxeldata(chunk, x, y, z, meshData);
-//                 }
-//             }
-//         }
-//     }
-//
-//     return meshData;
-// }
-
-// MeshData Voxeldata(Chunk chunk, int x, int y, int z, MeshData meshData) {
-//     Pos pos = new Pos(x, y, z);
-//
-//     foreach (var dir in DirectionUtils.Directions) {
-//         Pos touchingPos = pos + dir;
-//
-//         // Build the face if there is no touching cube or if is the side of the chunk.
-//         if (chunk.IsOutOfRange(touchingPos) || chunk.GetVoxel(touchingPos).IsAir) {
-//             VoxelGeometry.CreateVertexFace(meshData, pos, dir);
-//             VoxelGeometry.AddQuadTriangles(meshData);
-//
-//             Vector2 colorUv = colorTexture.GetColorUV(
-//                 chunk.GetVoxel(pos).color
-//             );
-//
-//             meshData.uv.Add(colorUv);
-//             meshData.uv.Add(colorUv);
-//             meshData.uv.Add(colorUv);
-//             meshData.uv.Add(colorUv);
-//         }
-//     }
-//
-//     return meshData;
-// }
