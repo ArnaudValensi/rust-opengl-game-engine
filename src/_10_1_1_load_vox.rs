7db7b3b7b16b0a74992ca17944d28680ad8ee887
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-06-05 23:06:49 +0200

    Clean useless code

--- src/_10_1_1_load_vox.rs
+++ src/_10_1_1_load_vox.rs
@@ -10,10 +10,6 @@ use self::glutin::{
     GlContext,
 };
 
-use self::gl::types::*;
-
-use std::ptr;
-use std::mem;
 use std::os::raw::c_void;
 use std::path::Path;
 use std::ffi::CStr;
@@ -82,7 +78,7 @@ fn run() -> Result<()> {
     // ------------------------------
     let mut window = Window::new(SCR_WIDTH, SCR_HEIGHT);
 
-    let (ourShader, VBO, VAO, texture1, texture2, cubePositions) = unsafe {
+    let (ourShader, texture1, texture2) = unsafe {
         // configure global opengl state
         // -----------------------------
         gl::Enable(gl::DEPTH_TEST);
@@ -92,83 +88,6 @@ fn run() -> Result<()> {
         let ourShader = Shader::new(
             "src/shaders/7.2.camera.vs",
             "src/shaders/7.2.camera.fs");
-
-        // set up vertex data (and buffer(s)) and configure vertex attributes
-        // ------------------------------------------------------------------
-        let vertices: [f32; 180] = [
-             -0.5, -0.5, -0.5,  0.0, 0.0,
-              0.5, -0.5, -0.5,  1.0, 0.0,
-              0.5,  0.5, -0.5,  1.0, 1.0,
-              0.5,  0.5, -0.5,  1.0, 1.0,
-             -0.5,  0.5, -0.5,  0.0, 1.0,
-             -0.5, -0.5, -0.5,  0.0, 0.0,
-
-             -0.5, -0.5,  0.5,  0.0, 0.0,
-              0.5, -0.5,  0.5,  1.0, 0.0,
-              0.5,  0.5,  0.5,  1.0, 1.0,
-              0.5,  0.5,  0.5,  1.0, 1.0,
-             -0.5,  0.5,  0.5,  0.0, 1.0,
-             -0.5, -0.5,  0.5,  0.0, 0.0,
-
-             -0.5,  0.5,  0.5,  1.0, 0.0,
-             -0.5,  0.5, -0.5,  1.0, 1.0,
-             -0.5, -0.5, -0.5,  0.0, 1.0,
-             -0.5, -0.5, -0.5,  0.0, 1.0,
-             -0.5, -0.5,  0.5,  0.0, 0.0,
-             -0.5,  0.5,  0.5,  1.0, 0.0,
-
-              0.5,  0.5,  0.5,  1.0, 0.0,
-              0.5,  0.5, -0.5,  1.0, 1.0,
-              0.5, -0.5, -0.5,  0.0, 1.0,
-              0.5, -0.5, -0.5,  0.0, 1.0,
-              0.5, -0.5,  0.5,  0.0, 0.0,
-              0.5,  0.5,  0.5,  1.0, 0.0,
-
-             -0.5, -0.5, -0.5,  0.0, 1.0,
-              0.5, -0.5, -0.5,  1.0, 1.0,
-              0.5, -0.5,  0.5,  1.0, 0.0,
-              0.5, -0.5,  0.5,  1.0, 0.0,
-             -0.5, -0.5,  0.5,  0.0, 0.0,
-             -0.5, -0.5, -0.5,  0.0, 1.0,
-
-             -0.5,  0.5, -0.5,  0.0, 1.0,
-              0.5,  0.5, -0.5,  1.0, 1.0,
-              0.5,  0.5,  0.5,  1.0, 0.0,
-              0.5,  0.5,  0.5,  1.0, 0.0,
-             -0.5,  0.5,  0.5,  0.0, 0.0,
-             -0.5,  0.5, -0.5,  0.0, 1.0
-        ];
-        // world space positions of our cubes
-        let cubePositions: [Vector3<f32>; 10] = [vec3(0.0, 0.0, 0.0),
-                                                 vec3(2.0, 5.0, -15.0),
-                                                 vec3(-1.5, -2.2, -2.5),
-                                                 vec3(-3.8, -2.0, -12.3),
-                                                 vec3(2.4, -0.4, -3.5),
-                                                 vec3(-1.7, 3.0, -7.5),
-                                                 vec3(1.3, -2.0, -2.5),
-                                                 vec3(1.5, 2.0, -2.5),
-                                                 vec3(1.5, 0.2, -1.5),
-                                                 vec3(-1.3, 1.0, -1.5)];
-        let (mut VBO, mut VAO) = (0, 0);
-        gl::GenVertexArrays(1, &mut VAO);
-        gl::GenBuffers(1, &mut VBO);
-
-        gl::BindVertexArray(VAO);
-
-        gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
-        gl::BufferData(gl::ARRAY_BUFFER,
-                       (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                       &vertices[0] as *const f32 as *const c_void,
-                       gl::STATIC_DRAW);
-
-        let stride = 5 * mem::size_of::<GLfloat>() as GLsizei;
-        // position attribute
-        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
-        gl::EnableVertexAttribArray(0);
-        // texture coord attribute
-        gl::VertexAttribPointer(1, 2, gl::FLOAT, gl::FALSE, stride, (3 * mem::size_of::<GLfloat>()) as *const c_void);
-        gl::EnableVertexAttribArray(1);
-
 
         // load and create a texture
         // -------------------------
@@ -233,7 +152,7 @@ fn run() -> Result<()> {
         let projection: Matrix4<f32> = perspective(Deg(FOV), SCR_WIDTH as f32 / SCR_HEIGHT as f32, 0.1, 100.0);
         ourShader.setMat4(c_str!("projection"), &projection);
 
-        (ourShader, VBO, VAO, texture1, texture2, cubePositions)
+        (ourShader, texture1, texture2)
     };
 
     // let chunk = VoxLoader::new();
@@ -299,18 +218,6 @@ fn run() -> Result<()> {
             ourShader.setMat4(c_str!("model"), &model);
 
             chunk_mesh.Draw(&ourShader);
-
-
-            // render boxes
-            // gl::BindVertexArray(VAO);
-            // for (_i, position) in cubePositions.iter().enumerate() {
-            //     // calculate the model matrix for each object and pass it to shader before drawing
-            //     let mut model: Matrix4<f32> = Matrix4::from_translation(*position);
-            //     model = model * Matrix4::from_axis_angle(vec3(1.0, 0.3, 0.5).normalize(), Deg(0.0));
-            //     ourShader.setMat4(c_str!("model"), &model);
-            //
-            //     gl::DrawArrays(gl::TRIANGLES, 0, 36);
-            // }
         }
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -320,12 +227,13 @@ fn run() -> Result<()> {
         window.gl_window.swap_buffers().unwrap();
     }
 
+    // TODO: clean meshes VAO, VBO
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    unsafe {
-        gl::DeleteVertexArrays(1, &VAO);
-        gl::DeleteBuffers(1, &VBO);
-    }
+    // unsafe {
+    //     gl::DeleteVertexArrays(1, &VAO);
+    //     gl::DeleteBuffers(1, &VBO);
+    // }
 
     Ok(())
 }
