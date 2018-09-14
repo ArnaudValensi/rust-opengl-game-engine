282c5da4690edaaac9a67abcf555634c9a62a971
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-09-15 00:28:51 +0200

    Move print_errors_and_exit function to errors module

--- src/errors.rs
+++ src/errors.rs
@@ -6,3 +6,19 @@ mod errors {
 }
 
 pub use self::errors::*;
+
+pub fn print_errors_and_exit(e: &Error) {
+    println!("error: {}", e);
+
+    for e in e.iter().skip(1) {
+        println!("caused by: {}", e);
+    }
+
+    // The backtrace is not always generated. Try to run this example
+    // with `RUST_BACKTRACE=1`.
+    if let Some(backtrace) = e.backtrace() {
+        println!("backtrace: {:?}", backtrace);
+    }
+
+    ::std::process::exit(1);
+}
--- src/game.rs
+++ src/game.rs
@@ -142,22 +142,6 @@ fn run() -> Result<()> {
     Ok(())
 }
 
-fn print_errors_and_exit(e: &Error) {
-    println!("error: {}", e);
-
-    for e in e.iter().skip(1) {
-        println!("caused by: {}", e);
-    }
-
-    // The backtrace is not always generated. Try to run this example
-    // with `RUST_BACKTRACE=1`.
-    if let Some(backtrace) = e.backtrace() {
-        println!("backtrace: {:?}", backtrace);
-    }
-
-    ::std::process::exit(1);
-}
-
 pub fn game() {
     if let Err(ref e) = run() {
         print_errors_and_exit(e);
