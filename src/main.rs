b3b082f678d26bc1f8e175b7cd31d36144f3fe09
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-09-15 14:20:26 +0200

    Add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate bigseed;
 extern crate pretty_env_logger;
+extern crate dotenv;
 
 use bigseed::game;
+use dotenv::dotenv;
 
 fn main() {
+    dotenv().ok();
     pretty_env_logger::init();
+
     game();
 }
