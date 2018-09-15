fa614fbaae957124fe3f9a2457941065f5e20ddb
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-09-16 01:25:18 +0200

    Remove some comments

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,5 @@
 use failure::Error;
 
-// TODO: Implement this style:
-// ```
-// error: could not download file from 'https://static.rust-lang.org/rustup/release-stable.toml' to '/tmp/rustup-update.T7Ry8gjuIytx/release-stable.toml'
-// info: caused by: error during download
-// info: caused by: [7] Couldn't connect to server (Failed to receive SOCKS4 connect request ack.)
-// ```
 pub fn print_errors_and_exit(error: &Error) {
     let mut causes = error.iter_chain();
 
