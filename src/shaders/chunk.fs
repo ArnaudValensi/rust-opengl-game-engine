7fdbfd2ca3d1be8d7e3dd023e00d8b336077b44b
Arnaud Valensi <arnaud.valensi@gmail.com>
2020-10-12 16:54:54 +0200

    Clean some code

--- src/shaders/chunk.fs
+++ src/shaders/chunk.fs
@@ -10,19 +10,18 @@ uniform vec3 ambientLightColor;
 uniform vec3 lightPosition;
 uniform vec3 lightColor;
 
-void main()
-{
-	float ambientStrength = 0.1;
+void main() {
+    float ambientStrength = 0.1;
 
-	// Ambient light calculation
+    // Ambient light calculation
     vec3 ambient = ambientStrength * ambientLightColor;
 
-	// Diffuse light calculation
-	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(lightPosition - FragPos);
-	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = diff * lightColor;
+    // Diffuse light calculation
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPosition - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
 
-	vec3 result = (ambient + diffuse) * Color;
+    vec3 result = (ambient + diffuse) * Color;
     FragColor = vec4(result, 1.0);
 }
--- src/shaders/chunk.vs
+++ src/shaders/chunk.vs
@@ -1,7 +1,7 @@
 #version 330 core
-layout (location = 0) in vec3 aPosition;
-layout (location = 1) in uint aColorIndex;
-layout (location = 2) in vec3 aNormal;
+layout(location = 0) in vec3 aPosition;
+layout(location = 1) in uint aColorIndex;
+layout(location = 2) in vec3 aNormal;
 
 out vec3 FragPos;
 out vec3 Color;
@@ -14,10 +14,9 @@ uniform mat4 projection;
 // TODO: A vec3 could be used
 uniform vec4 palette[256];
 
-void main()
-{
-	gl_Position = projection * view * model * vec4(aPosition, 1.0f);
-	FragPos = vec3(model * vec4(aPosition, 1.0));
-	Color = palette[aColorIndex].xyz;
-	Normal = normalMatrix * aNormal;
+void main() {
+    gl_Position = projection * view * model * vec4(aPosition, 1.0f);
+    FragPos = vec3(model * vec4(aPosition, 1.0));
+    Color = palette[aColorIndex].xyz;
+    Normal = normalMatrix * aNormal;
 }
